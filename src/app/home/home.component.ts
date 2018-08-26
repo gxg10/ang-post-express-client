72fb357139d64d87bdfa08cd02b545d3c8199989
Brande George <george.brande@gmail.com>
2018-08-26 20:43:31 +0300

    file upload in progress

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,14 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ElementRef, Input } from '@angular/core';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpHeaders, HttpEventType, HttpRequest, HttpErrorResponse, HttpEvent } from '@angular/common/http';
+
+
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Response } from '@angular/http';
+
+const URL = 'http://localhost:8080/api/';
+
 
 @Component({
   selector: 'app-home',
@@ -8,9 +17,12 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]
 })
 export class HomeComponent implements OnInit {
-  
+  fileToUpload: File = null;
+
   constructor(config:NgbCarouselConfig,
-  @Inject('baseUrl') private baseUrl) { 
+  @Inject('baseUrl') private baseUrl,
+  private http: HttpClient,
+  private el: ElementRef) {
     config.interval = 1000;
     config.wrap = true;
     config.keyboard = true;
@@ -20,5 +32,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+
+  
 
 }
