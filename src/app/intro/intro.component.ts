ad9766f8ff3ab1a506926860e76ca6a7f3683a73
Brande George <george.brande@gmail.com>
2018-08-30 19:31:03 +0300

    stiri - doar titlu

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NewsService } from '../services/news.service';
 import { Stiri } from '../shared/stiri';
-import {MatPaginatorModule, PageEvent, MatPaginator} from '@angular/material/paginator';
-
 
 @Component({
   selector: 'app-intro',
@@ -10,26 +8,11 @@ import {MatPaginatorModule, PageEvent, MatPaginator} from '@angular/material/pag
   styleUrls: ['./intro.component.css']
 })
 export class IntroComponent implements OnInit {
-  displayedColumns: string[] = ['titlu', 'continut'];
+   displayedColumns: string[] = ['titlu'];
 
   stiri: Stiri[];
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-
-  pageIndex: number;
-  length = 6;
-  pageSize = 3;
-  pageSizeOptions: number[] = [1, 3, 6];
-  currentPage = 0;
-  pageEvent: PageEvent;
-
-
-  setPageSizeOptions(setPageSizeOptionsInput: string) {
-    this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
-  }
-
   // dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
-
   constructor(private newsService: NewsService) { }
 
   ngOnInit(): void {
